c2edfd82e0c82054c288def77231403ed8cb55d7
Kenneth Norris <kjnorris@gmail.com>
2018-06-05 19:45:09 -0400

    HW1 Final
    Final submission for homework - 40/40 points. Moved random placement to
    main method from constructor.

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -2,51 +2,41 @@ import java.util.*;
 	
 public class WhackAMole {
 	
-	public int score, molesLeft, attemptsLeft;
-	public char [][] moleGrid;
+	int score = 0;
+	int molesLeft = 0;
+	int attemptsLeft;
+	char [][] moleGrid;
 	
 	WhackAMole (int numAttempts, int gridDimension) {
-		int numMoles = 10;
-		int x, y;
-		Random rand = new Random();
-		
+
 		this.attemptsLeft = numAttempts;
 		this.moleGrid = new char[gridDimension][gridDimension];
 		this.score = 0;
-		this.molesLeft = 10;
 		
 		for (int i = 0; i < moleGrid.length; i++) {
 			for (int j = 0; j < moleGrid[i].length; j++) {
 				moleGrid[i][j] = '*';
 			}
 		}
-		
-		while (numMoles > 0) {
-			x = rand.nextInt(this.moleGrid.length);
-			y = rand.nextInt(this.moleGrid[x].length);
-			if (place(x, y)) {
-				numMoles--;
-			}
-		}
 	}
 	
 	public boolean place (int x, int y) {
-		boolean retValue;
 		if (this.moleGrid[x][y] != 'M') {
 			this.moleGrid[x][y] = 'M';
-			retValue = true;
+			this.molesLeft++;
+			return true;
 		} else {
-			retValue = false;
+			return false;
 		}
-		return retValue;
 	}
 	
-	public void printGrid() {
-		for (int i = 0; i < this.moleGrid.length; i++) {
-			for (int j = 0; j < this.moleGrid[i].length; j++) {
-				System.out.print(this.moleGrid[i][j] + " ");
-			}
-			System.out.print("\n");
+	public void whack(int x, int y) {
+		this.attemptsLeft--;
+		
+		if (this.moleGrid[x][y] == 'M') {
+			this.moleGrid[x][y] = 'W';
+			this.molesLeft--;
+			this.score++;
 		}
 	}
 	
@@ -63,26 +53,34 @@ public class WhackAMole {
 		}
 	}
 	
-	public void whack(int x, int y) {
-		this.attemptsLeft--;
-		
-		if (this.moleGrid[x][y] == 'M') {
-			this.moleGrid[x][y] = 'W';
-			this.molesLeft--;
-			this.score++;
+	public void printGrid() {
+		for (int i = 0; i < this.moleGrid.length; i++) {
+			for (int j = 0; j < this.moleGrid[i].length; j++) {
+				System.out.print(this.moleGrid[i][j] + " ");
+			}
+			System.out.print("\n");
 		}
 	}
 	
 	public static void main(String[] args) {
-		
-		final int GRID_SIZE = 10;
-		final int WHACKS_ALLOWED = 50;
-		
-		Scanner sc = new Scanner(System.in);
-		int whackX, whackY;
+
+		int x, y;
+		int numMoles = 10;
 		String[] splitArray;
-		WhackAMole wam = new WhackAMole(WHACKS_ALLOWED, GRID_SIZE);
 		
+		Random rand = new Random();
+		Scanner sc = new Scanner(System.in);
+		
+		WhackAMole wam = new WhackAMole(50, 10);
+		
+		while (numMoles > 0) {
+			x = rand.nextInt(wam.moleGrid.length);
+			y = rand.nextInt(wam.moleGrid[x].length);
+			if (wam.place(x, y)) {
+				numMoles--;
+			}
+		}
+
 		sc.useDelimiter("\\D\\s*");
 		
 		while (wam.attemptsLeft > 0 && wam.molesLeft > 0) {
@@ -92,18 +90,20 @@ public class WhackAMole {
 			
 			splitArray = sc.nextLine().split("\\s*(,|\\s)\\s*");
 			
-			whackX = Integer.parseInt(splitArray[0]);
-			whackY = Integer.parseInt(splitArray[1]);
+			x = Integer.parseInt(splitArray[0]);
+			y = Integer.parseInt(splitArray[1]);
 
-			if (whackX == -1 && whackY == -1) {
+			if (x == -1 && y == -1) {
 				wam.attemptsLeft = 0;
-			} else if (whackX >= 0 && whackX <= 9 && whackY >= 0 && whackY <=9){
-				wam.whack(whackX,  whackY);
+			} else if (x >= 0 && x <= 9 && y >= 0 && y <=9){
+				wam.whack(x,  y);
 				wam.printGridToUser();
 			} else {
 				System.out.println("Please enter coordinates between 0 and 9");
 			}
 		}
+		
+		sc.close();
 
 		System.out.println("\n");
 		System.out.println("GAME OVER");
